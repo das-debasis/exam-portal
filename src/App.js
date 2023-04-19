14c9a20aa7611933ce20529b5c3593258456f405
asteriskdotexe <shubhamred227@gmail.com>
2023-04-19 14:03:15 +0530

    Register added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "./components/openform.css";
 import "./components/Landing.css";
 import { Routes, Route } from "react-router-dom";
 import { Landing } from "./components/Landing";
+import 'bootstrap/dist/css/bootstrap.css';
+import {Register} from './components/Register';
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/openforum" element={<ThoughtList />} />
         <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,101 +2,61 @@ import React from 'react'
 
 export const Register = () => {
   return (
-    <div>
-    <center>
-      <div class="header">
-        <nav>
-          <a href="index.html">e-Exam</a>
-          <a href="openform.html">Openforum</a>
-          <a href="">e-Repository</a>
-          <a href="">MyWall</a>
-        </nav>
-        <div class="form-input">
-          <input type="text" class="search_bar" />
-          <button type="submit" class="search_btn">
-            <i class="fa-solid fa-magnifying-glass"></i>
-          </button>
-        </div>
-        <div class="login">
-          <nav class="login">
-            <a href="register.html">SIGNUP</a>
-          </nav>
-        </div>
-      </div>
-      <div
-        style="
-          text-align: center;
-          margin-top: 70px;
-          margin-bottom: 70px;
-          border-radius: 10px;
-          background-color: rgba(52, 70, 143, 0.103);
-          width: 35%;
-          padding-bottom: 40px;
-        "
-      >
-        <h1 style="padding-top: 30px">Register</h1>
+      <div style={{
+        margin: "70px auto",
+        fontFamily: "Poppins",
+        textAlign: 'center',
+        borderRadius: '10px',
+        backgroundColor: 'rgba(52, 70, 143, 0.103)',
+        width: "40%",}}>
+        <h1 style={{paddingTop: "40px"}}>Register</h1>
         <table>
           <tr>
             <th>
               <div
-                style="padding: 20px 10px 0px 40px; text-align: left"
-                id="name"
+                style={{padding: "40px 0px 0px 40px", textAlign: "left", fontWeight: "bold"}}
               >
                 <label for="fname">First Name : </label>
-                <input type="text" class="form-control" placeholder="Prajosh" />
+                <input type="text" className="form-control" placeholder="Prajuice" style={{width:"110%"}} />
               </div>
             </th>
             <th>
               <div
-                style="padding: 20px 40px 0px 10px; text-align: left"
-                id="name"
+                style={{padding: "40px 0px 0px 0px", textAlign: "left", fontWeight: "bold", marginLeft:"45px"}}
+                
               >
                 <label for="fname">Last Name : </label>
-                <input type="text" class="form-control" placeholder="Sahoo" />
+                <input type="text" className="form-control" placeholder="Sahoo" style={{width:"110%"}} />
               </div>
             </th>
           </tr>
         </table>
-        <div style="padding: 20px 40px 0px 40px; text-align: left">
+        <div style={{padding: "20px 40px 0px 40px", textAlign: "left", fontWeight: "bold"}}>
           <label for="email">Email : </label>
-          <input type="email" class="form-control" placeholder="abc@xyz.com" />
+          <input type="email" className="form-control" placeholder="abc@xyz.com" />
         </div>
-        <div style="padding: 20px 40px 0px 40px; text-align: left">
+        <div style={{padding: "20px 40px 0px 40px", textAlign: "left", fontWeight: "bold"}}>
           <label for="username">Contact : </label>
           <table>
             <tr>
               <th>
-                <label style="font-weight: 400;margin-right: 15px"">+91</label>
+                <label style={{marginRight: "15px", fontWeight: "normal"}}>+91</label>
               </th>
-              <th style="width: 130%;"">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="123456789"
-                />
+              <th style={{width: "130%"}}>
+                <input type="text" className="form-control" placeholder="123456789"/>
               </th>
             </tr>
           </table>
+          <div style={{padding: "20px 0px 0px 0px", textAlign: "left", fontWeight: "bold"}}>
+            <label for="username">Organization : </label>
+            <input type="text" className="form-control" placeholder="SIT, BBSR" />
+          </div>
+          <div style={{padding: "20px 0px 40px 0px", textlign: "left", fontWeight: "bold"}}>
+            <label for="username">Password : </label>
+            <input type="password" className="form-control" placeholder="Password" />
+          </div>
+          <button type="button" className="btn btn-dark" style={{width: "100%", marginBottom: "40px"}}>Register</button>
         </div>
-        <div style="padding: 20px 40px 0px 40px; text-align: left">
-          <label for="username">Organization : </label>
-          <input type="text" class="form-control" placeholder="SIT, BBSR" />
-        </div>
-        <div style="padding: 20px 40px 40px 40px; text-align: left">
-          <label for="username">Password : </label>
-          <input type="password" class="form-control" placeholder="Password" />
-        </div>
-        <button
-          type="button"
-          class="btn btn-dark"
-          onclick="location.href ='index1.html';"
-          style="width: 84%"
-        >
-          Register
-        </button>
       </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    </center>
-    </div>
-  )
+  );
 }
